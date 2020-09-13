710470f36ccc85ee0ba4df4234da378db491c6e6
annisa343 <71145062+annisa343@users.noreply.github.com>
2020-09-13 23:53:45 +0700

    Add files via upload

---
+++ UTS.java
@@ -0,0 +1,19 @@
+package ulangan;
+
+public class UTS {
+	public static void main(String[] args) {
+		String A = "ANNISA";
+		String B = "AULIA";
+		String C = "NURSABILA";
+		System.out.println(A+"-"+B+"-");
+		
+		if (C.contains("NUR")) {
+			System.out.println(C);
+		}
+		else {
+			System.out.println("tidak ada");
+		}
+		
+		
+	}
+}
